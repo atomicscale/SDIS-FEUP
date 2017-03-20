6cdd4f5ed040a871de31e29c379ff58ffa4e72b0
AtomicScale <up201303104@fe.up.pt>
2017-03-20 00:18:48 +0000

    Chunk starting

---
+++ Proj_01/src/FileSystem/Chunk.java
@@ -0,0 +1,40 @@
+package FileSystem;
+
+public class Chunk {
+    private String fileId;
+    private int chunkNo;
+    private int replicationDeg;
+    private byte[] data;
+
+    public Chunk (String fileId, int chunkNo, int replicationDeg,byte[] data){
+        this.fileId = fileId;
+        this.chunkNo = chunkNo;
+        this.replicationDeg = replicationDeg;
+        this.data = data;
+    }
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public int getChunkNo() {
+        return chunkNo;
+    }
+
+    public int getReplicationDeg() {
+        return replicationDeg;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void incReplicationDeg() {
+        replicationDeg = replicationDeg + 1;
+    }
+
+    public void decReplicationDeg(){
+        replicationDeg = replicationDeg - 1;
+    }
+
+}
