6440a0573353814f7ac48c4dc69667f11bbc07cf
AtomicScale <up201303104@fe.up.pt>
2017-03-25 01:55:40 +0000

    Message Class initialization and method to generate SHA-256 hash partially implemented

--- Proj_01/src/Message/Message.java
+++ Proj_01/src/Message/Message.java
@@ -1,9 +1,39 @@
 package Message;
 
-public class Message{
+import java.security.MessageDigest;
+import java.nio.charset.StandardCharsets;
+
+public class Message {
     public static int CHUNK_MAX_SIZE = 64000;
 
-    public Message(){
+    private String header;
+    private byte[] body;
 
+
+    public Message() {
+
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public byte[] getBody() {
+        return body;
+    }
+
+    public void setBody(byte[] body) {
+        this.body = body;
+    }
+
+    public String generateHash(String fileID){
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(fileID.getBytes(StandardCharsets.UTF_8));
+
+        //TODO from hash to String;
     }
 }
